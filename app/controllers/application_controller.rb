e1ffa7553f5aa5145448119312f258669cad6d59
Ambar Khare <ambar.khare24@gmail.com>
2022-05-07 19:03:45 -0700

    commit tree 2bd4a192ab0d3620579db5adf402638e6c52d3eb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+        erb :team
+    end
+
+    post '/team' do
+        @team_name = params[:team][:name]
+        @team_motto = params[:team][:motto]
+        @hero1 = params[:team][:heros][0]
+        @hero2 = params[:team][:heros][1]
+        @hero3 = params[:team][:heros][2]
+        @heros = [@hero1, @hero2, @hero3]
+        erb :super_hero
+    end
 
 end
